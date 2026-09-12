f1bd3701f3c6bfc2e2b07c730c2a5bd65bced202
Mordaft <alvaro.cabezas.morera@gmail.com>
2026-09-12 15:08:54 +0200

    optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   build: {
+    chunkSizeWarningLimit: 1000,
     rolldownOptions: {
       output: {
         codeSplitting: {
@@ -11,27 +12,6 @@ export default defineConfig({
               name: 'dexie',
               test: /[\\/]node_modules[\\/]dexie/,
               priority: 10,
-            },
-            {
-              name: 'langchain-core',
-              test: /[\\/]node_modules[\\/]@langchain[\\/]core/,
-              maxSize: 400 * 1024,
-              priority: 9,
-            },
-            {
-              name: 'langsmith',
-              test: /[\\/]node_modules[\\/]langsmith/,
-              priority: 8,
-            },
-            {
-              name: 'zod',
-              test: /[\\/]node_modules[\\/]zod/,
-              priority: 7,
-            },
-            {
-              name: 'langgraph',
-              test: /[\\/]node_modules[\\/]@langchain[\\/]langgraph/,
-              priority: 6,
             },
           ],
         },
